27bd0721baba7bd68a282103f63fa0bb56f1acdd
Jositomb <jmarcosmb@gmail.com>
2026-09-21 18:18:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "mi-servicio-v15301";
+const CACHE = "mi-servicio-v15601";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./styles-v27.css?v=15301",
-  "./app-v27.js?v=15301",
+  "./styles-v27.css?v=15601",
+  "./app-v27.js?v=15601",
   "./icon-apple.png",
   "./manifest.webmanifest",
   "./core/config.js",
